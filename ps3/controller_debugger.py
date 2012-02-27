9afa4c21249ffa79dc40f87c80e6402d91991a31
Sander van de Graaf <mail@svdgraaf.nl>
2012-02-27 23:59:15 +0100

    controller debugger

---
+++ ps3/controller_debugger.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import pygame
+from pygame.locals import *
+import usb.core
+import time
+ 
+# init joystick
+pygame.init()
+j = pygame.joystick.Joystick(0)
+j.init()
+
+print 'Initialized Joystick : %s' % j.get_name()
+print 'axis: %s, buttons: %s' % (j.get_numaxes(), j.get_numbuttons())
+
+# threshold for (analog) axis
+threshold = 0.60
+
+try:
+    while True:
+        for event in pygame.event.get():
+            if event.type == JOYAXISMOTION:
+                if abs(event.value) > threshold:
+                    print event
+            if event.type == JOYBUTTONUP:
+                print event
+            if event.type == JOYBUTTONDOWN:
+                print event
+except KeyboardInterrupt:
+    j.quit()
