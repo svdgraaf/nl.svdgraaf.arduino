4a97e077093a1bddcdcce955b53407ecbec3d2f5
Sander van de Graaf <mail@svdgraaf.nl>
2012-02-28 21:52:01 +0100

    added analog stick schema

--- ps3/controller_debugger.py
+++ ps3/controller_debugger.py
@@ -13,14 +13,16 @@ print 'Initialized Joystick : %s' % j.get_name()
 print 'axis: %s, buttons: %s' % (j.get_numaxes(), j.get_numbuttons())
 
 # threshold for (analog) axis
-threshold = 0.60
+threshold = 0.1
 
 try:
     while True:
         for event in pygame.event.get():
             if event.type == JOYAXISMOTION:
                 if abs(event.value) > threshold:
-                    print event
+                    if event.axis == 0:
+                        print event
+                        
             if event.type == JOYBUTTONUP:
                 print event
             if event.type == JOYBUTTONDOWN:
